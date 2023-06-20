4e629ef68d0ea9a4f9be7960ee2da37d7832a620
aden herold <adenherold@DESKTOP-LRQH2FQ.localdomain>
2023-06-20 20:59:10 +1000

    test(project-1): added provided unit tests

--- project-1/src/bin/kvs.rs
+++ project-1/src/bin/kvs.rs
@@ -1,7 +1,12 @@
 use clap::Parser;
-use kvs::Args;
+use kvs::{Args, Command};
 
 fn main() {
     let args = Args::parse();
-    println!("{args:?}");
+
+    match args.command {
+        Command::Set { .. } => panic!("unimplemented"),
+        Command::Get { .. } => panic!("unimplemented"),
+        Command::Rm { .. } => panic!("unimplemented"),
+    };
 }
