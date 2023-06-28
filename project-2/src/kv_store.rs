8c90665775098589508a9ecc64dd75562fa3a7b7
aden herold <adenherold@DESKTOP-LRQH2FQ.localdomain>
2023-06-28 23:21:47 +1000

    refactor: updated log_table hashmap to reference new LogPointer struct
    refactored to use combination of offset and length in LogPointer, to remove need for Frame struct when parsing

--- project-2/src/kv_store.rs
+++ project-2/src/kv_store.rs
@@ -1,12 +1,11 @@
-use crate::Frame;
 use anyhow::{bail, Context, Result};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs::{self, File, OpenOptions};
-use std::io::{BufReader, BufWriter, Seek, SeekFrom, Write};
+use std::io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write};
 use std::path::Path;
 
-type LogTable = HashMap<String, u64>;
+type LogTable = HashMap<String, LogPointer>;
 
 #[derive(Serialize, Deserialize, Debug)]
 enum Command {
@@ -23,18 +22,16 @@ pub struct KvStore {
 impl KvStore {
     pub fn set(&mut self, key: &str, value: &str) -> Result<()> {
         let command = Command::Set(key.into(), value.into());
-        let offset = self.file.stream_position().unwrap();
-
-        self.append_to_log(command)?;
-        self.log_table.insert(key.into(), offset);
+        let pointer = self.append_to_log(command)?;
+        self.log_table.insert(key.into(), pointer);
 
         Ok(())
     }
 
     pub fn get(&self, key: &str) -> Result<Option<String>> {
         match self.log_table.get(key) {
-            Some(offset) => {
-                let value = self.read_value_from_log(*offset)?;
+            Some(pointer) => {
+                let value = self.read_value_from_log(pointer)?;
                 Ok(Some(value))
             }
             None => Ok(None),
@@ -45,7 +42,6 @@ impl KvStore {
         self.log_table.remove(key).context("Key not found")?;
 
         let command = Command::Rm(key.into());
-
         self.append_to_log(command)?;
 
         Ok(())
@@ -67,37 +63,32 @@ impl KvStore {
         Ok(Self { log_table, file })
     }
 
-    fn append_to_log(&self, command: Command) -> Result<()> {
-        let serialized = bincode::serialize(&command)?;
-
-        let frame = Frame::new(&serialized).encode();
+    fn append_to_log(&self, command: Command) -> Result<LogPointer> {
         let mut writer = BufWriter::new(&self.file);
+        let offset = writer.stream_position()?;
+        let serialized = bincode::serialize(&command)?;
+        let length = serialized.len();
 
         writer
-            .write_all(&frame)
+            .write_all(&serialized)
             .context("Failed to append serialized command to log")?;
 
-        Ok(())
+        Ok(LogPointer { offset, length })
     }
 
-    fn read_value_from_log(&self, offset: u64) -> Result<String> {
+    fn read_value_from_log(&self, pointer: &LogPointer) -> Result<String> {
+        let mut buffer = vec![0u8; pointer.length];
         let mut reader = BufReader::new(&self.file);
-        reader.seek(SeekFrom::Start(offset)).unwrap();
-        let decoded = Frame::decode(&mut reader)?;
 
-        let result = match decoded {
-            Some(frame) => {
-                let command = bincode::deserialize::<Command>(&frame.bytes)?;
+        reader.seek(SeekFrom::Start(pointer.offset))?;
+        reader.read_exact(&mut buffer)?;
 
-                match command {
-                    Command::Set(_, value) => Ok(value),
-                    _ => bail!("Unable to parse command from log"),
-                }
-            }
-            None => bail!("Value not found in log at offset {}", offset),
+        let result = match bincode::deserialize::<Command>(&buffer)? {
+            Command::Set(_, value) => Ok(value),
+            _ => bail!("Unable to parse command from log"),
         };
 
-        reader.seek(SeekFrom::End(0)).unwrap();
+        reader.seek(SeekFrom::End(0))?;
 
         result
     }
@@ -106,23 +97,25 @@ impl KvStore {
 fn build_log_table(file: &mut File) -> Result<LogTable> {
     let mut log_table = HashMap::new();
     let mut reader = BufReader::new(file);
+    let mut offset = reader.seek(SeekFrom::Start(0))?;
 
-    loop {
-        let offset = reader.stream_position()?;
-        let decoded = Frame::decode(&mut reader)?;
+    while let Ok(cmd) = bincode::deserialize_from::<_, Command>(&mut reader) {
+        let pos = reader.stream_position()?;
+        let length = (pos - offset) as usize;
 
-        match decoded {
-            Some(frame) => {
-                let command = bincode::deserialize::<Command>(&frame.bytes)?;
+        match cmd {
+            Command::Set(key, _) => log_table.insert(key, LogPointer { offset, length }),
+            Command::Rm(key) => log_table.remove(&key),
+        };
 
-                match command {
-                    Command::Set(key, _) => log_table.insert(key, offset),
-                    Command::Rm(key) => log_table.remove(&key),
-                };
-            }
-            None => break,
-        }
+        offset = reader.seek(SeekFrom::Current(0))?;
     }
 
     Ok(log_table)
+}
+
+#[derive(Debug)]
+struct LogPointer {
+    offset: u64,
+    length: usize,
 }
