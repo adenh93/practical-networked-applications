0c3cc4c33f19645395aed53f165ae4d67cb3bf3d
adenh93 <aden.herold1@gmail.com>
2024-01-29 10:00:04 +1100

    refactor: removed ReadBuffer type in favour of wrapping buffer Vector in std::io::Cursor type
    ReadBuffer type is redundant, as std::io::Cursor implements Read, and performs the same cursor duties

--- building-blocks/bb-2/src/bin/exercise-3-vec.rs
+++ building-blocks/bb-2/src/bin/exercise-3-vec.rs
@@ -1,17 +1,20 @@
 use anyhow::Result;
-use bb_2::{Move, ReadBuffer};
+use bb_2::Move;
 use fake::{Fake, Faker};
+use std::io::{Cursor, Seek};
 
 const NUM_OF_MOVES: usize = 1_000;
 
 fn main() -> Result<()> {
-    let mut buffer = ReadBuffer::new();
+    let mut buffer = Cursor::new(vec![]);
 
     for _ in 0..NUM_OF_MOVES {
         let rand_move: Move = Faker.fake();
         let document = bson::to_document(&rand_move)?;
         document.to_writer(&mut buffer)?;
     }
+
+    buffer.seek(std::io::SeekFrom::Start(0))?;
 
     while let Ok(de_move) = bson::from_reader::<_, Move>(&mut buffer) {
         println!("{de_move:?}");
--- building-blocks/bb-2/src/lib.rs
+++ building-blocks/bb-2/src/lib.rs
@@ -1,6 +1,5 @@
 use fake::Dummy;
 use serde::{Deserialize, Serialize};
-use std::io::{self, Read, Write};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Dummy)]
 pub enum Move {
@@ -8,50 +7,4 @@ pub enum Move {
     Right(u8),
     Down(u8),
     Left(u8),
-}
-
-#[derive(Debug)]
-pub struct ReadBuffer {
-    bytes: Vec<u8>,
-    cursor: usize,
-}
-
-impl ReadBuffer {
-    pub fn new() -> Self {
-        Self {
-            bytes: Vec::new(),
-            cursor: 0,
-        }
-    }
-}
-
-impl Default for ReadBuffer {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl Write for ReadBuffer {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.bytes.write(buf)
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        self.bytes.flush()
-    }
-}
-
-impl Read for ReadBuffer {
-    fn read(&mut self, mut buf: &mut [u8]) -> std::io::Result<usize> {
-        if self.cursor == self.bytes.len() {
-            return Err(io::ErrorKind::UnexpectedEof)?;
-        }
-
-        let bytes_to_read = buf.len().min(self.bytes.len() - self.cursor);
-        let slice = &self.bytes[self.cursor..self.cursor + bytes_to_read];
-        buf.write_all(slice)?;
-        self.cursor += bytes_to_read;
-
-        Ok(bytes_to_read)
-    }
 }
